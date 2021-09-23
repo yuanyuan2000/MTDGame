6ef7d2fd9275680838f6b7aa898efd2280e9b93e
Alex Brown <alex.brown.r@gmail.com>
2021-09-23 17:14:23 +0800

    Still trying to fix things...

--- mtdnetwork/azure/scripts/startup.sh
+++ mtdnetwork/azure/scripts/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt update;
-sudo apt install -y python3.8 python3.8-dev;
+sudo apt install -y build-essential libssl-dev libffi-dev rustc python3.8 python3.8-dev;
 sudo apt install -y python3-pip;
-sudo python3.8 -m pip install pip;
+sudo python3.8 -m pip install --upgrade pip;
 sudo python3.8 -m pip install setuptools setuptools-rust numpy Cython;
 git clone https://github.com/Ccamm/MTDSim mtdsim;
 cd mtdsim;
