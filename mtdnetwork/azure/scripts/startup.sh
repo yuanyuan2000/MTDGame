cbed336d42c45ed56266e3f116b894740fbc2c22
Alex Brown <alex.brown.r@gmail.com>
2021-09-23 16:18:58 +0800

    Try fixing startup script

--- mtdnetwork/azure/scripts/startup.sh
+++ mtdnetwork/azure/scripts/startup.sh
@@ -2,6 +2,8 @@
 
 sudo apt update;
 sudo apt install -y python3.8 python3.8-dev;
+python3.8 -m pip install pip;
+python3.8 -m pip install setuptools setuptools-rust numpy;
 git clone https://github.com/Ccamm/MTDSim mtdsim;
 cd mtdsim;
 sudo python3.8 setup.py install;
